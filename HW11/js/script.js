f929558e9d439ccf4b35b8a7239e72742dcebc10
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2020-10-29 15:36:06 +0200

    HW11
    HW11

---
+++ HW11/js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+
+
+
+const obj1 = {
+     0: "zero",
+     42: "answer",
+     "greeting": "Hello",
+     3.14: "PI",
+}
+
+
+function getKeys(obj1) {
+/*
+     if (0 !=String) {
+          console.log(obj1[0]);
+     }
+     if (42 != String) {
+          console.log(obj1[42]);
+     }
+     if (3.14 != String) {
+          console.log(obj1[3.14]);
+     }
+     if ("greeting" != String) {
+          console.log(obj1["Hello"]);
+     }
+     if ("greeting" != String) {
+          console.log(obj1["Hello"]);
+     }
+*/
+
+     switch (Number !=String) {
+          case "greeting" != String:
+               console.log(obj1["Hello"]);
+          case 0 !=String:
+               console.log(obj1[0]);
+          case 42 != String:
+               console.log(obj1[42]);
+          case 3.14 != String:
+               console.log(obj1[3.14]);
+     }
+
+}
+
+
+console.log(getKeys(obj1));
